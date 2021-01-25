9c38b2e82fa345d01251d46d0503d51c50c1aa2f
Thaynan F. Freitas <65410847+thaynanff@users.noreply.github.com>
2021-01-24 21:33:46 -0300

    New Solutions

---
+++ 6kyu/Replace With Alphabet Position.py
@@ -0,0 +1,30 @@
+'''
+Problem description:
+
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabet_position("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+'''
+
+def alphabet_position(text):
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+    "w", "x", "y", "z"]
+    out = []
+    new=[]
+    for l in text:
+        if l.isalpha():
+            new.append(l.lower())
+    for l in new:
+        out.append(str(alphabet.index(l)+1))
+    new.clear()
+    new = " ".join(out)
+    return new
---
+++ 6kyu/Sum of Digits-Digital Root.py
@@ -0,0 +1,24 @@
+'''
+Problem description:
+
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+'''
+
+def digital_root(n):
+    soma = 0
+    while True:
+        for c in str(n):
+            soma += int(c)
+        if soma < 10:
+            break
+        n = int(soma)
+        soma = 0
+    return soma
