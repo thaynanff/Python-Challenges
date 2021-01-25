0a07846a73b830336ab6457603c511c6e6d17bb9
Thaynan F. Freitas <65410847+thaynanff@users.noreply.github.com>
2021-01-24 21:26:25 -0300

    .

---
+++ 7kyu/Binary-Addition.py
@@ -0,0 +1,8 @@
+'''
+Problem description:
+
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+'''
+
