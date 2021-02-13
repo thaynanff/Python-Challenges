00d7d074c2aec802849271516d95161498e72d55
Thaynan F. Freitas <65410847+thaynanff@users.noreply.github.com>
2021-02-12 23:06:35 -0300

    Create Find the missing letter.py

---
+++ 6kyu/Find the missing letter.py
@@ -0,0 +1,27 @@
+'''
+Problem description:
+
+Find the missing letter
+
+Write a method that takes an array of consecutive (increasing) letters as input and 
+that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. 
+The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+
+["a","b","c","d","f"] -> "e"
+["O","Q","R","S"] -> "P"
+'''
+
+def find_missing_letter(chars):
+    missingChar = ''
+    for i in range(0,len(chars)-1):
+        if(ord(chars[i+1]) - ord(chars[i]) > 1):
+            missingChar = chr(ord(chars[i])+1)
+
+    return missingChar
