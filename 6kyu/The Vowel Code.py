b6861b59a67a5946c1307eb55a5d4e96756b0d66
Thaynan F. Freitas <65410847+thaynanff@users.noreply.github.com>
2021-02-20 19:24:09 -0300

    Create The Vowel Code.py

---
+++ 6kyu/The Vowel Code.py
@@ -0,0 +1,34 @@
+'''
+Problem description:
+
+Step 1: Create a function called encode() to replace all the lowercase vowels in a 
+given string with numbers according to the following pattern:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+
+For example, encode("hello") would return "h2ll4". There is no need to worry about 
+uppercase vowels in this kata.
+
+Step 2: Now create a function called decode() to turn the numbers back into vowels 
+according to the same pattern shown above.
+
+For example, decode("h3 th2r2") would return "hi there".
+
+For the sake of simplicity, you can assume that any numbers passed into the function 
+will correspond to vowels.
+'''
+
+alf = 'aeiou'
+def encode(st):
+    for i, e in enumerate(alf):
+        st = st.replace(e, str(i+1))
+    return st
+    
+def decode(st):
+    for i, e in enumerate(alf):
+        st = st.replace(str(i+1), e)
+    return st
