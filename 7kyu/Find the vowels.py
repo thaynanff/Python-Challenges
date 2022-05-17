05b7151b540b1feabe34c5ec15368f6b585b2a7a
Thaynan F. Freitas <65410847+thaynanff@users.noreply.github.com>
2022-05-16 22:48:25 -0300

    Create Find the vowels.py

---
+++ 7kyu/Find the vowels.py
@@ -0,0 +1,19 @@
+'''
+We want to know the index of the vowels in a given word, for example, there are two vowels in 
+the word super (the second and fourth letters).
+
+So given a string "super", we should return a list of [2, 4].
+
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+NOTES
+Vowels in this context refers to: a e i o u y (including upper case)
+This is indexed from [1..n] (not zero indexed!)
+
+'''
+
+def vowel_indices(word):
+    return [ind+1 for ind, value in enumerate(word.lower()) if value in ['a','e','i','o','u','y']]
